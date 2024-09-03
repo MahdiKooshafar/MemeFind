2c5f3b39e137ef977a87f5e94aa7fd8cdc47d874
MahdiKooshafar <mkooshafar2@gmail.com>
2024-09-02 20:48:03 -0700

    set base

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,24 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { LoadingComponent } from "./components/tests/loading/loading.component";
+import { CommonModule } from '@angular/common';
+import { NavComponent } from './components/tests/nav/nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, LoadingComponent , CommonModule , NavComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'Memefind';
+  loading : boolean = true;
+  mainapp : boolean = false; 
+  ngOnInit(){
+    setTimeout(() => {
+      this.loading = false;
+      this.mainapp = true ;
+    }, 2000);
+  }
 }
