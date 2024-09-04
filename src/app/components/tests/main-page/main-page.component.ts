3d4becb78e6acba630d6535686ad35e35c250dd4
MahdiKooshafar <mkooshafar2@gmail.com>
2024-09-04 07:56:51 -0700

    init Score

--- src/app/components/tests/main-page/main-page.component.ts
+++ src/app/components/tests/main-page/main-page.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject } from '@angular/core';
 import { TelegramWebappService } from '@zakarliuka/ng-telegram-webapp';
 import { TelegramServiceService } from './Servises/telegram-service.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-main-page',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   providers: [TelegramWebappService , TelegramServiceService],
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css'
@@ -16,45 +17,41 @@ export class MainPageComponent {
   firstName: any;
   lastName: any;
 
-  Scores : number = 0 ;
-
+  score: number = 0 ; 
+  Scores : string[] = ["score"] ;
   question1 : any ;
 
+
+  
 
   ngOnInit() {
     this.firstName = this.telServise.initDataUnsafe.user?.first_name;
     this.lastName = this.telServise.initDataUnsafe.user?.last_name;
     this.question1 = this.telServise.initDataUnsafe.user?.id;
-    console.log(this.question1)
-    // this.telServise.cloudStorage.getItem()
-    try{
-      
-    }
-    catch{
-      
-    }
+    this.telServise.webApp.CloudStorage.getItem(this.Scores[0] , (e , value) => {
+      if(e){
+        console.log(e)
+      }else{
+        console.log("its call back log" , value);
+        this.score = Number(value)
+
+      }
+    } )
     
-    // this.WhatsIs = this.firstName.toString() + this.lastName.toString();
   }
-  setScore(){
-    this.telServise.cloudStorage.setItem(this.question1.toString() , this.Scores.toString())
-
+  seeScore(){
+    
   }
-
-  updateScore(userId: number, score: number) {
-    this.scoreServise.setGameScore(userId, score).subscribe(response => {
-      console.log('Score updated:', response);
-    });
+  TopScore(){
+    this.score = this.score + 1
+    this.telServise.webApp.CloudStorage.setItem(this.Scores[0] , (this.score).toString())
+    this.telServise.webApp.CloudStorage.getItem(this.Scores[0] , (e , value) => {
+      if(e){
+        console.log(e)
+      }else{
+        console.log("its call back log" , value);
+        this.score = Number(value)
+      }
+    } )
   }
-
-  getHighScores(userId: number, lscore: number) {
-    this.scoreServise.getGameHighScores(userId, lscore).subscribe(highScores => {
-      console.log('High scores:', highScores);
-    });
-  }
-
-  // shareScore() {
-  //   TelegramGameProxy.shareScore();
-  // }
-  
 }
